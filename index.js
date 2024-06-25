40ef040d3853979c6b3d5f76dd11fa1db131de5b
BryanMartinez152 <bryanwladimir8@gmail.com>
2024-06-24 20:51:39 -0500

    CORS 2

--- index.js
+++ index.js
@@ -13,23 +13,26 @@ app.use(express.json());
 conectarDB();
 
 //Cors
-const dominiosPermitidos = [process.env.FRONTEND_URL]
+// const dominiosPermitidos = [process.env.FRONTEND_URL,'*']
 
-const corsOptions ={
-    origin: function(origin,callback){
-        if (dominiosPermitidos.indexOf(origin) !== -1 ) {
-            //El origen del request esta permitidoS
-            callback(null,true);
-        }else{
-            callback(new Error('No permitido por CORS'))
-        }
-    }
-}
+// const corsOptions ={
+//     origin: function(origin,callback){
+//         if (dominiosPermitidos.indexOf(origin) !== -1 ) {
+//             //El origen del request esta permitidoS
+//             callback(null,true);
+//         }else{
+//             callback(new Error('No permitido por CORS'))
+//         }
+//     }
+// }
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/api/veterinarios',veterinarioRoutes);
 app.use('/api/pacientes',pacienteRouter);
+app.use('/prueba',(req,res)=>{
+    res.json({msg: 'Si funcciona'})
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT,()=>{
